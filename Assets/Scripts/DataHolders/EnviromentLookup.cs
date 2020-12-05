dfcc451f9e8c69de4fca8aba9d4b8ac474131dd8
Gonzako <al341522@uji.es>
2020-12-05 19:37:51 +0100

    Added tileset and some collision logic

---
+++ Assets/Scripts/DataHolders/EnviromentLookup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/// 
+/// Copyright (c) 2020 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+[CreateAssetMenu(menuName ="Enviroment/LookupTable")]
+public class EnviromentLookup : ScriptableObject
+{
+
+    #region PublicFields
+    
+    public HashSet<Tile> _Obstacles;
+    public HashSet<Tile> _Unwalkables;
+    public HashSet<Tile> _Traps;
+    public HashSet<Tile> _Movables;
+    #endregion
+
+    #region PrivateFields
+
+    [SerializeField]
+    private List<Tile> PickableObstacles;
+
+    [SerializeField]
+    private List<Tile> Unwalkable;
+
+    [SerializeField]
+    private List<Tile> Traps;
+
+    [SerializeField]
+    private List<Tile> Movables;
+    #endregion
+
+    #region UnityCallBacks
+    private void Awake()
+    {
+        _Unwalkables = new HashSet<Tile>(Unwalkable);
+        _Obstacles = new HashSet<Tile>(PickableObstacles);
+        _Traps = new HashSet<Tile>(Traps);
+        _Movables = new HashSet<Tile>(Movables);
+    }
+
+
+    #endregion
+
+    #region PublicMethods
+
+    #endregion
+
+
+    #region PrivateMethods
+
+    #endregion
+}
--- Assets/heroAI.cs
+++ Assets/heroAI.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
+using DG.Tweening;
 /// 
 /// Copyright (c) 2020 All Rights Reserved
 ///
@@ -15,9 +16,33 @@ public class heroAI : MonoBehaviour
 {
 
     #region PublicFields
+    public ScriptableObjectArchitecture.FloatVariable waitTime;
+
+    public static event Action onCharacterMove;
     #endregion
 
     #region PrivateFields
+    private const float dashTime = 0.2f;
+
+    [SerializeField]
+    Direction currentDir = Direction.Forward;
+
+    [SerializeField]
+    Transform visuals = null;
+
+    [SerializeField]
+    EnviromentLookup table = null;
+    [SerializeField]
+    Grid gridHolder = null;
+    [SerializeField]
+    Tilemap obstaclesTilemap = null;
+    [SerializeField]
+    Tilemap backgroundTilemap = null;
+
+    private Tile currentGround { get { return backgroundTilemap.GetTile(backgroundTilemap.WorldToCell(transform.position)) as Tile; } }
+    private Tile nextGround { get { return backgroundTilemap.GetTile(backgroundTilemap.WorldToCell(transform.position+currentDir.toVector3())) as Tile; } }
+
+    private Tile nextObstacle { get { return backgroundTilemap.GetTile(obstaclesTilemap.WorldToCell(transform.position + currentDir.toVector3())) as Tile; } }
     #endregion
 
     #region UnityCallBacks
@@ -43,10 +68,88 @@ public class heroAI : MonoBehaviour
 
     #region PublicMethods
 
+
+
     #endregion
 
 
     #region PrivateMethods
 
+    private IEnumerator moveCicle()
+    {
+        yield return new WaitForSeconds(waitTime.Value);
+        //Collision check
+
+        var tile = nextGround;
+        var obstacle = nextObstacle;
+        int i = 0;
+        while (i < 4 && ((tile ? table._Unwalkables.Contains(tile) : true) || (obstacle ? !table._Obstacles.Contains(obstacle) : false)))
+        {
+            currentDir += 1;
+            currentDir = (Direction)((int)currentDir % 4);
+            tile = nextGround;
+            i++;
+        }
+
+
+        if(i > 4)
+        {
+            Debug.Log("AI trapped, dont move");
+        }
+        else
+        {
+            yield return moveCharacter();
+        }
+        //Decide movement
+
+
+
+
+        yield return moveCicle();
+    }
+
+    private IEnumerator moveCharacter()
+    {
+        transform.position += currentDir.toVector3();
+        Tween t = visuals.DOMove(transform.position, dashTime).SetEase(Ease.InOutQuint);
+        t.SetAutoKill(false);
+        t.Play();
+        yield return new WaitUntil(()=>!t.active);
+        t.Kill();
+        visuals.position = transform.position;
+    }
+
     #endregion
+}
+
+
+public enum Direction
+{
+    Forward, Right, Backwards, Left
+}
+
+public static class DirectionExtentions
+{
+
+    public static Vector3 toVector3(this Direction n)
+    {
+        
+        switch (n)
+        {
+            case Direction.Forward:
+                return Vector3.forward;
+                break;
+            case Direction.Backwards:
+                return Vector3.back;
+                break;
+            case Direction.Left:
+                return Vector3.left;
+                break;
+            case Direction.Right:
+                return Vector3.right;
+                break;
+        }
+        Debug.LogError("Enum Switch Not Working");
+        return Vector3.forward;
+    }
 }
