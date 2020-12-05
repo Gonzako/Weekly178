76c61e8ee89ed516ac78631edbcd970c0b924148
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-12-05 17:23:40 +0200

    Some setup.

---
+++ Assets/Scripts/UI/SlideBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideBall : MonoBehaviour
+{
+    [SerializeField] private Sprite _default;
+    [SerializeField] private Sprite _Active;
+
+    [SerializeField] private Image _img;
+
+    private bool isBallActive = false;
+
+    public void SetActiveSlide(bool value)
+    {
+        isBallActive = value;
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (isBallActive)
+        {
+            setImageSprite(_Active);
+        }else if (!isBallActive)
+        {
+            setImageSprite(_default);
+        }
+    }
+
+    private void setImageSprite(Sprite sprite)
+    {
+        if (_img.sprite == sprite) return;
+        _img.sprite = sprite;
+    }
+}
---
+++ Assets/Scripts/UI/SlideView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using TMPro;
+
+public class SlideView : MonoBehaviour
+{
+    [SerializeField] private SlideDetails _details;
+    [SerializeField] private VideoPlayer _player;
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private void OnEnable()
+    {
+        if (_details._clip == null || _details._description == null) return;
+
+        _player.clip = _details._clip;
+        _text.text = _details._description;
+    }
+}
+
+public struct SlideDetails
+{
+    public VideoClip _clip;
+    public string _description;
+}
