c5a8669c26b91c88b392764686cb226fc3c64446
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-12-06 01:06:09 +0200

    UI + Score Tracking

---
+++ Assets/Scripts/Game/ScoreTracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class ScoreTracker : MonoBehaviour
+{
+    [SerializeField] private IntVariable _Score = null;
+    [SerializeField] private FloatVariable _timer = null;
+    [SerializeField] private bool _shouldTrack = true;
+
+    [SerializeField] private const int basescore = 10000;
+    [SerializeField] private const int multiplier = 10;
+
+    private void Start()
+    {
+        _timer.Value = 0F;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_shouldTrack == false) return;
+        _timer.Value += Time.deltaTime;
+    }
+
+    public void CalculateScore()
+    {
+        _shouldTrack = false;
+        _Score.Value = basescore / (int)_timer * multiplier;
+    }
+}
---
+++ Assets/Scripts/UI/ValueDisplayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ScriptableObjectArchitecture;
+
+public class ValueDisplayer : MonoBehaviour
+{
+    [SerializeField] private string _displayFormat = "The current price is {0} per ounce.";
+    [SerializeField] private Text _text;
+    [SerializeField] private BaseVariable[] _variables;
+
+
+    public void SetValueToText()
+    {
+        string rs = string.Format(_displayFormat, _variables);
+        rs = rs.Replace("<br>", "\n");
+        _text.text = rs;
+    }   
+}
