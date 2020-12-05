73bbc4218adba7ef95fb3721b502395e47bd9385
Gonzako <al341522@uji.es>
2020-12-05 16:31:53 +0100

    added pre logic AI script

---
+++ Assets/heroAI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 
+/// Copyright (c) 2020 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+
+public class heroAI : MonoBehaviour
+{
+
+    #region PublicFields
+    #endregion
+
+    #region PrivateFields
+    #endregion
+
+    #region UnityCallBacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnEnable()
+    {
+ 
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    #endregion
+
+
+    #region PrivateMethods
+
+    #endregion
+}
