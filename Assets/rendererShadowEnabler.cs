220b489987ba1652804ff6f2ca7dbcba8de4ac5d
Gonzako <al341522@uji.es>
2020-12-06 13:47:31 +0100

    Made obstacle cast shadows on background

---
+++ Assets/rendererShadowEnabler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 
+/// Copyright (c) 2020 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+
+public class rendererShadowEnabler : MonoBehaviour
+{
+
+    #region PublicFields
+    #endregion
+
+    #region PrivateFields
+    #endregion
+
+    #region UnityCallBacks
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        var rend = GetComponent<Renderer>();
+        rend.receiveShadows = true;
+        rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnEnable()
+    {
+ 
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    #endregion
+
+
+    #region PrivateMethods
+
+    #endregion
+}
